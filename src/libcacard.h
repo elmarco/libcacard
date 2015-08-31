9d964d66a3e7ecb8628b4957dd0b72e411cdaf37
Marc-André Lureau <marcandre.lureau@redhat.com>
2015-08-31 20:55:01 +0200

    Add libcacard.h
    A top-level header file is often easier to deal with for programs
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

---
+++ src/libcacard.h
@@ -0,0 +1,22 @@
+/*
+ * This work is licensed under the terms of the GNU LGPL, version 2.1 or later.
+ * See the COPYING file in the top-level directory.
+ */
+#ifndef LIBCACARD_H
+#define LIBCACARD_H
+
+#include "cac.h"
+#include "capcsc.h"
+#include "card_7816.h"
+#include "card_7816t.h"
+#include "eventt.h"
+#include "vcard_emul.h"
+#include "vcard_emul_type.h"
+#include "vcard.h"
+#include "vcardt.h"
+#include "vevent.h"
+#include "vreader.h"
+#include "vreadert.h"
+#include "vscard_common.h"
+
+#endif
